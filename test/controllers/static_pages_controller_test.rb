c02c8e8b1721c19c7c54edaa44f26c289dcacb60
josphat-kariuki <me_ziraq@yahoo.com>
2019-08-22 22:17:18 +0300

    Completed guard setup for automatic testing

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -27,6 +27,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select 'title', "About | #{@base_title}"
   end
+  
   test 'should get contact page' do
     get static_pages_contact_url
     assert_response :success
